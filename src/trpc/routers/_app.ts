edc6734f8656d12296e39aa258f7a2cdfe96b1a4
Aditya Goel <adityagoel.abvb@gmail.com>
2026-08-27 01:02:34 +0530

    Added Meetings Form, Filter, DataPage

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,12 @@
-import { agentsRouter } from '@/modules/agents/server/procedures';
 import { createTRPCRouter } from '../init';
+import { agentsRouter } from '@/modules/agents/server/procedures';
+import { meetingsRouter } from '@/modules/meetings/server/procedures';
+
  
 export const appRouter = createTRPCRouter({
   //Here are all the operations my application exposes through tRPC.
   agents: agentsRouter,
+  meetings: meetingsRouter,
 });
  
 // export type definition of API
